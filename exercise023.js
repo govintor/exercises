f914a85fb7f20aec8aca1150b60235b3f6adc2e4
George Rodriguez <govintor@icloud.com>
2022-07-03 19:51:56 -0400

    solution CW 23

---
+++ exercise023.js
@@ -0,0 +1,7 @@
+function findShort(s){
+  return Math.min.apply(null, s.split(' ').map(w => w.length));
+}
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
