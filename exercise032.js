03d80c2cde8c5b9db1978cf36bd8157ac12e3bf7
George Rodriguez <govintor@icloud.com>
2022-07-11 19:23:23 -0400

    solution cw 032

---
+++ exercise032.js
@@ -0,0 +1,14 @@
+function find_average(array) {
+  if (array.length === 0) {
+  return 0;
+  }
+  var result = 0;
+  for (i=0; i<array.length; i++) {
+    result +=array[i];
+  }
+  return result/array.length;
+}
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
