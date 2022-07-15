ce2f0a1f064e18b4e31ba03e345a210c1d9b22ab
George Rodriguez <govintor@icloud.com>
2022-07-15 19:23:42 -0400

    solution 044 cw

---
+++ exercise044.js
@@ -0,0 +1,11 @@
+function switchItUp(number){
+  var words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  return words[number];
+}
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
