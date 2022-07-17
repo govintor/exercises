9cd8fc949bde30e9f9a701b79fcff699fa60c21d
George Rodriguez <govintor@icloud.com>
2022-07-17 19:38:56 -0400

    solution cw 046

---
+++ exercise046.js
@@ -0,0 +1,11 @@
+function shortcut (string) {
+ return string.replace(/[aeiou]/ig,'')
+}
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
