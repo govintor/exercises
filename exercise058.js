df9f7848979416b77dcdc05738ea7c09fa71dea9
George Rodriguez <govintor@icloud.com>
2022-07-31 18:17:52 -0400

    another solution for the same exercise 058

--- exercise058.js
+++ exercise058.js
@@ -1,7 +1,7 @@
 function minMax(arr){
   return [Math.min(...arr), Math.max(...arr)];
 }
-
+const minMax = arr => [ Math.min(...arr), Math.max(...arr) ];
 // Story
 // Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
 
