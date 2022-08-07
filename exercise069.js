056b2a884fcf569e75161dcd82045229db1ca2ae
George Rodriguez <govintor@icloud.com>
2022-08-07 18:02:24 -0400

    " alternative solution included"

--- exercise069.js
+++ exercise069.js
@@ -5,3 +5,10 @@ function smallEnough(a, limit){
 // You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
 
 // You can assume all values in the array are numbers.
+
+function smallEnough(a, limit){
+    for(var i of a){
+      if(i > limit)
+        return false;
+    }
+    return true;
