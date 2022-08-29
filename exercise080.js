5285eda4a639fd9641207bc898fe1c8a02f2d064
George Rodriguez <govintor@icloud.com>
2022-08-29 18:40:06 -0400

    adding explanation cw 080

--- exercise080.js
+++ exercise080.js
@@ -3,3 +3,12 @@ function abbrevName(name){
  return name.split(" ").map((part) => part[0].toUpperCase()).join(".")   // code away
 
 }
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
