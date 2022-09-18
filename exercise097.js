1b41bf65aead449a21c5ee5ea69e1fc0083ed552
George Rodriguez <govintor@icloud.com>
2022-09-18 19:47:51 -0400

    solution of CW 097

---
+++ exercise097.js
@@ -0,0 +1,10 @@
+let age = prompt('age?', 18);
+
+let message = (age < 3) ? 'Hi, baby!' :
+  (age < 18) ? 'Hello!' :
+  (age < 100) ? 'Greetings!' :
+  'What an unusual age!';
+
+alert( message );
+
+// practicing ternary operator
